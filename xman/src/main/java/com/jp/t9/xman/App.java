ec68c6eaeabd755ca10d5aa63aba64a0a7f9c8ab
ron <liuliangron@gmail.com>
2019-02-23 10:30:24 +0900

    first commit

---
+++ xman/src/main/java/com/jp/t9/xman/App.java
@@ -0,0 +1,13 @@
+package com.jp.t9.xman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
