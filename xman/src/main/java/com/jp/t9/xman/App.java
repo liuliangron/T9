deb12fa8bc858be61e54d438cdffd2cec9affaa4
ron <ron@rons-mbp>
2019-02-23 11:44:03 +0900

    upgrade higher version of eclipse.jdt.core from 3.6 to 3.16 support AST.JLS11

--- xman/src/main/java/com/jp/t9/xman/App.java
+++ xman/src/main/java/com/jp/t9/xman/App.java
@@ -18,79 +18,76 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
  * Hello world!
  *
  */
-public class App 
-{
-	//use ASTParse to parse string
-		public static void parse(String str) {
-			ASTParser parser = ASTParser.newParser(AST.JLS3);
-			parser.setSource(str.toCharArray());
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			parser.setUnitName("/Users/ron/git/T9/xman/src/main/java/com/jp/t9/xman/App.java");
-	 
-			final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-	 
-			cu.accept(new ASTVisitor() {
-	 
-				Set<String> names = new HashSet<String>();
-	 
-		
-				public boolean visit(VariableDeclarationFragment node) {
-					SimpleName name = node.getName();
-					this.names.add(name.getIdentifier());
-					System.out.println("Declaration of '" + name + "' at line"
-							+ cu.getLineNumber(name.getStartPosition()));
-					return false; // do not continue 
-				}
-	 
-				public boolean visit(SimpleName node) {
-					if (this.names.contains(node.getIdentifier())) {
-						System.out.println("Usage of '" + node + "' at line "
-								+ cu.getLineNumber(node.getStartPosition()));
-					}
-					return true;
-				}
-			});
-	 
-		}
-	 
-		//read file content into a string
-		public static String readFileToString(String filePath) throws IOException {
-			StringBuilder fileData = new StringBuilder(1000);
-			BufferedReader reader = new BufferedReader(new FileReader(filePath));
-	 
-			char[] buf = new char[10];
-			int numRead = 0;
-			while ((numRead = reader.read(buf)) != -1) {
-				System.out.println(numRead);
-				String readData = String.valueOf(buf, 0, numRead);
-				fileData.append(readData);
-				buf = new char[1024];
+public class App {
+	// use ASTParse to parse string
+	public static void parse(String str) {
+		ASTParser parser = ASTParser.newParser(AST.JLS11);
+		parser.setSource(str.toCharArray());
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setUnitName("/Users/ron/git/T9/xman/src/main/java/com/jp/t9/xman/App.java");
+
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+		cu.accept(new ASTVisitor() {
+
+			Set<String> names = new HashSet<String>();
+
+			public boolean visit(VariableDeclarationFragment node) {
+				SimpleName name = node.getName();
+				this.names.add(name.getIdentifier());
+				System.out.println("Declaration of '" + name + "' at line" + cu.getLineNumber(name.getStartPosition()));
+				return false; // do not continue
 			}
-	 
-			reader.close();
-	 
-			return  fileData.toString();	
+
+			public boolean visit(SimpleName node) {
+				if (this.names.contains(node.getIdentifier())) {
+					System.out.println("Usage of '" + node + "' at line " + cu.getLineNumber(node.getStartPosition()));
+				}
+				return true;
+			}
+		});
+
+	}
+
+	// read file content into a string
+	public static String readFileToString(String filePath) throws IOException {
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+		char[] buf = new char[10];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			System.out.println(numRead);
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
 		}
-	 
-		//loop directory to get file list
-		public static void ParseFilesInDir() throws IOException{
-			//File dirs = new File(".");
-			//String dirPath = dirs.getCanonicalPath() + File.separator+"src"+File.separator;
-	       //String dirpath="D:\programs\workspace\strategy\src\"; 
-			//File root = new File(dirpath);
-			//System.out.println(root.listFiles());
-			//File[] files = root.listFiles ( );
-			String filePath = "/Users/ron/git/T9/xman/src/main/java/com/jp/t9/xman/App.java";
-	 
-			// for (File f : files ) {
-				// filePath = f.getAbsolutePath();
-				 //if(f.isFile()){
-					 parse(readFileToString(filePath));
-				 //}
-			 //}
-		}
-	 
-		public static void main(String[] args) throws IOException {
-			ParseFilesInDir();
-		}
+
+		reader.close();
+
+		return fileData.toString();
+	}
+
+	// loop directory to get file list
+	public static void ParseFilesInDir() throws IOException {
+		// File dirs = new File(".");
+		// String dirPath = dirs.getCanonicalPath() +
+		// File.separator+"src"+File.separator;
+		// String dirpath="D:\programs\workspace\strategy\src\";
+		// File root = new File(dirpath);
+		// System.out.println(root.listFiles());
+		// File[] files = root.listFiles ( );
+		String filePath = "/Users/ron/git/T9/xman/src/main/java/com/jp/t9/xman/App.java";
+
+		// for (File f : files ) {
+		// filePath = f.getAbsolutePath();
+		// if(f.isFile()){
+		parse(readFileToString(filePath));
+		// }
+		// }
+	}
+
+	public static void main(String[] args) throws IOException {
+		ParseFilesInDir();
+	}
 }
